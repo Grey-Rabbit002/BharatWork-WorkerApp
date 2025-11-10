55dbcc8b115f3c230c934cce94968ad18dd354e1
Arjun Saxena <rabbit.404.saxena@gmail.com>
2025-11-10 22:09:04 +0530

    minor change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
-    id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // FlutterFire configuration plugin (Google Services)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -19,15 +18,12 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.bharatwork"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = flutter.minSdkVersion // 👈 Important: Firebase Auth needs at least 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -35,19 +31,16 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-    dependencies {
-    implementation 'com.google.firebase:firebase-auth'
-}
 }
 
 flutter {
     source = "../.."
 }
 
-apply plugin: 'com.google.gms.google-services'
-
+dependencies {
+    implementation("com.google.firebase:firebase-auth:22.3.1") // 👈 use explicit version
+    // Other Firebase SDKs can be added here as needed
+}
